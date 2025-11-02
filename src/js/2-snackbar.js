df0ac54c1e9beb7f55320fbe4a70062bae60221e
shadberry22 <malinovska.julia@gmail.com>
2025-11-02 16:13:11 +0100

    finish task 2

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,46 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state.value;
+
+  if (delay < 0) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Please enter a non-negative delay (ms)',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  const prom = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  prom
+    .then(delay => {
+      iziToast.success({
+        title: 'OK',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+});
